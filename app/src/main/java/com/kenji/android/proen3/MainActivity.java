aacce3c40853eca4ae5b4a51ead7f684fd1a443d
a1no <a1no2jz2l3@yahoo.co.jp>
2016-06-09 16:36:25 +0900

    Home画面に動的に配置するプログラムの一部を追加

--- app/src/main/java/com/kenji/android/proen3/MainActivity.java
+++ app/src/main/java/com/kenji/android/proen3/MainActivity.java
@@ -4,6 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +17,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //レイアウトの生成
+//        dynamicViewGeneration(15);
+
+
     }
+
+
+
+    private void dynamicViewGeneration(int GenerationNumber){
+        ScrollView sv = new ScrollView(this);
+        sv.setMinimumWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+        sv.setMinimumHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+        setContentView(sv);
+
+
+        LinearLayout linear_v = new LinearLayout(this);
+        linear_v.setOrientation(LinearLayout.VERTICAL);
+        sv.addView(linear_v, new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        for (int i=0; i<GenerationNumber; i++){
+            LinearLayout ll = new LinearLayout(this);
+            ll.setOrientation(LinearLayout.HORIZONTAL);
+            linear_v.addView(ll, new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            for (int j=0; j<3; j++){
+                Button imgBtn = new Button(this);
+                imgBtn.setText(i + "-" + j);
+                ll.addView(imgBtn, new RelativeLayout.LayoutParams(300, ViewGroup.LayoutParams.WRAP_CONTENT));
+            }
+        }
+
+    }
+
 
 
     public void aa(View v){
